938637c7ef05409fe06dd942db3d6e3ecc2271fe
Joao Benites <joao.alvesbenites@gmail.com>
2020-01-31 15:37:06 -0300

    COTI

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const body = require("body-parser");
+const cors = require('cors')
+const app = express();
+
+
+app.use(cors())
+app.use(body.json())
+app.use(expres.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/lista' , (req,res) =>{
+    mockLista = [
+        {
+            id:1 , nome:"Lu", email:'lu@gmail.com'
+        },
+        {
+            id:2 , nome:"Belem", email:'belem@gmail.com'
+        }
+        ]
+    res.json(mockLista)
+})
+
+
+app.listen(process.env.PORT || 4008)
